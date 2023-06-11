a3297a2f8c47bc81980f37af54f8023a31154aab
sergiohscl <sergiohscl2@gmail.com>
2023-06-11 10:20:18 -0300

    apresentaçao completa

--- app/Pessoa.py
+++ app/Pessoa.py
@@ -1,12 +1,17 @@
 import requests
 
 class Pessoa:
-    def __init__(self, nome):
+    def __init__(self, nome, sobrenome):
         self.nome = nome
+        self.sobrenome = sobrenome
+        self.dados_obtidos = False
     
     def dados(self):
         resposta = requests.get('https://jsonplaceholder.typicode.com/users/1')
 
         if resposta.sucess:
             return 'CONECTADO COM SUCESSO'
+        else:
+            self.dados_obtidos = False
+            return 'ERRO 404'
         
