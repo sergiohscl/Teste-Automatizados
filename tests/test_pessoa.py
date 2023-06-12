068309e7219e3bed9f45eed25a041c93fc123a91
sergiohscl <sergiohscl2@gmail.com>
2023-06-12 09:20:54 -0300

    ajustes

--- tests/test_pessoa.py
+++ tests/test_pessoa.py
@@ -1,16 +1,17 @@
-""" 
+"""
 class Pessoa
     __init__
         nome str
         sobrenome str
-        dados_obtidos bool (inicia false)
+        dados_obtidos bool (inicia False)
 
     API:
         obter_todos_os_dados -> method
-            Ok
+            OK
             404
-"""
 
+            (dados_obtidos se torna True se dados obtidos com sucesso)
+"""
 try:
     import sys
     import os
@@ -30,38 +31,70 @@ import unittest
 from unittest.mock import patch
 from Pessoa import Pessoa
 
+
 class TestPessoa(unittest.TestCase):
     def setUp(self):  # metodo para estanciar a classe antes do test
-        self.p1 = Pessoa('Sergio', 'Henrique')
+        self.p1 = Pessoa('Luiz', 'Otávio')
+        self.p2 = Pessoa('Maria', 'Miranda')
 
-    def test_pessoa_nome_correct(self):
-        self.assertEqual(self.p1.nome, 'Sergio')
-    
-    def test_pessoa_nome_str(self):
+    def test_pessoa_attr_nome_tem_o_valor_correto(self):
+        self.assertEqual(self.p1.nome, 'Luiz')
+        self.assertEqual(self.p2.nome, 'Maria')
+
+    def test_pessoa_attr_nome_e_str(self):
         self.assertIsInstance(self.p1.nome, str)
+        self.assertIsInstance(self.p2.nome, str)
 
-    def test_pessoa_sobrenome_correct(self):
-        self.assertEqual(self.p1.sobrenome, 'Henrique')
-    
-    def test_pessoa_sobrenome_str(self):
+    def test_pessoa_attr_sobrenome_tem_o_valor_correto(self):
+        self.assertEqual(self.p1.sobrenome, 'Otávio')
+        self.assertEqual(self.p2.sobrenome, 'Miranda')
+
+    def test_pessoa_attr_sobrenome_e_str(self):
         self.assertIsInstance(self.p1.sobrenome, str)
-    
-    def test_pessoa_dados_obtidos_false(self):
+        self.assertIsInstance(self.p2.sobrenome, str)
+
+    def test_pessoa_attr_dados_obtidos_inicia_false(self):
         self.assertFalse(self.p1.dados_obtidos)
-        
-    def test_obter_todos_os_dados_sucesso(self):
+        self.assertFalse(self.p2.dados_obtidos)
+
+    def test_obter_todos_os_dados_sucesso_OK(self):
         with patch('requests.get') as fake_request:
             fake_request.return_value.ok = True
 
-            self.assertEqual(self.p1.obter_todos_os_dados(), 'CONECTADO COM SUCESSO')
+            self.assertEqual(self.p1.obter_todos_os_dados(), 'CONECTADO')
             self.assertTrue(self.p1.dados_obtidos)
 
-    def test_obter_todos_os_dados_erro_404(self):
+            self.assertEqual(self.p2.obter_todos_os_dados(), 'CONECTADO')
+            self.assertTrue(self.p2.dados_obtidos)
+
+    def test_obter_todos_os_dados_falha_404(self):
         with patch('requests.get') as fake_request:
             fake_request.return_value.ok = False
 
             self.assertEqual(self.p1.obter_todos_os_dados(), 'ERRO 404')
             self.assertFalse(self.p1.dados_obtidos)
+
+            self.assertEqual(self.p2.obter_todos_os_dados(), 'ERRO 404')
+            self.assertFalse(self.p2.dados_obtidos)
+
+    def test_obter_todos_os_dados_sucesso_e_falha_sequencial(self):
+        with patch('requests.get') as fake_request:
+            fake_request.return_value.ok = True
+
+            self.assertEqual(self.p1.obter_todos_os_dados(), 'CONECTADO')
+            self.assertTrue(self.p1.dados_obtidos)
+
+            self.assertEqual(self.p2.obter_todos_os_dados(), 'CONECTADO')
+            self.assertTrue(self.p2.dados_obtidos)
+
+            fake_request.return_value.ok = False
+
+            self.assertEqual(self.p1.obter_todos_os_dados(), 'ERRO 404')
+            self.assertFalse(self.p1.dados_obtidos)
+
+            self.assertEqual(self.p2.obter_todos_os_dados(), 'ERRO 404')
+            self.assertFalse(self.p2.dados_obtidos)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2) # verbose saída detalhada
@@ -71,4 +104,4 @@ Pelo que entendi, isso é para determinar o código que é
 executado apenas quando o script é executado 
 diretamente/independentemente, não como um módulo 
 dentro de outro script.
-"""
+"""    
